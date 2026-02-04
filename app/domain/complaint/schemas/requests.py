8eb0040c7106bdfc3fffdf9f8f504298128a5fc2
Soru <chansolchoe9216@gmail.com>
2026-02-04 17:02:38 +0900

    fix: fix req validation error in complaint patch api

--- app/domain/complaint/schemas/requests.py
+++ app/domain/complaint/schemas/requests.py
@@ -5,5 +5,7 @@ from app.domain.complaint.models.complaint_model import ComplaintStep
 
 
 class UpdateComplaintRequest(BaseRequest):
-    name: str = Field(..., description="고소장 제목", examples=["고소장 제목"])
-    step: ComplaintStep = Field(..., description="고소장 단계", examples=[ComplaintStep.DOCUMENT])
+    name: str | None = Field(None, description="고소장 제목", examples=["고소장 제목"])
+    step: ComplaintStep | None = Field(
+        None, description="고소장 단계", examples=[ComplaintStep.DOCUMENT]
+    )
