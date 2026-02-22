3c9d6460e07ad8550cc0989c6af0adb43ccdf3e9
Soru <chansolchoe9216@gmail.com>
2026-02-22 12:57:37 +0900

    ci/cd: add prod branching in workflows

--- app/core/settings.py
+++ app/core/settings.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     )
 
     env: str = Field(default="local", alias="ENV")
-    WEB_APP_URL: str
+    WEB_APP_URL: str | None = None
     CORS_ORIGINS: list[str] = Field(
         default_factory=lambda: ["http://localhost:3000"], alias="CORS_ORIGINS"
     )
@@ -22,11 +22,11 @@ class Settings(BaseSettings):
 
     AWS_REGION: str
     AWS_PROFILE: str | None = None
-    COGNITO_CLIENT_ID: str
-    COGNITO_USER_POOL_ID: str
-    COGNITO_CLIENT_SECRET: str
-    COGNITO_DOMAIN: str
-    S3_BUCKET_NAME: str
+    COGNITO_CLIENT_ID: str | None = None
+    COGNITO_USER_POOL_ID: str | None = None
+    COGNITO_CLIENT_SECRET: str | None = None
+    COGNITO_DOMAIN: str | None = None
+    S3_BUCKET_NAME: str | None = None
 
 
 settings = Settings()
